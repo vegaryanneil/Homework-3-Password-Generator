2cc94646d851fae7779602ad51f53aed97b05b04
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-02-13 19:01:44 -0500

    Redid HTML and javascript code

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,27 @@
 // Assignment Code
-// For slider and box
-const characterAmountNumber = document.getElementById("characterAmountNumber");
-const passwordGeneratorForm = document.getElementById("passwordGeneratorForm");
-const passwordDisplay = document.getElementById("passwordGeneratorForm");
-const includeUppercaseElement = document.getElementById("includeUppercase");
-const includeNumbersElement = document.getElementById("includeNumbers");
-const includeSymbolsElement = document.getElementById("passwordDisplay");
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90);
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122);
-const NUMBERS_CHAR_CODES = arrayFromLowToHigh(48, 57);
-const SYMBOLS_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat( arrayFromLowToHigh(91, 96)).concat( arrayFromLowToHigh(123, 126))
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-console.log (LOWERCASE_CHAR_CODES)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
 
-characterAmountNumber.addEventListener("input", syncCharacterAmount);
-characterAmountRange.addEventListener("input", syncCharacterAmount);
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
 
-form.addEventListener("submit", e => {
+form.addEventListener('submit', e => {
   e.preventDefault()
   const characterAmount = characterAmountNumber.value
   const includeUppercase = includeUppercaseElement.checked
@@ -29,32 +34,30 @@ form.addEventListener("submit", e => {
 function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
   let charCodes = LOWERCASE_CHAR_CODES
   if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
-  if (includeNumbers) charCodes = charCodes.concat(NUMBERS_CHAR_CODES)
-  if (includeSymbols) charCodes = charCodes.concat(SYMBOLS_CHAR_CODES)
-  const passwordCharacters = [] 
-  for (let i = 0; i < characterAmount, i++;)
-  const characterCode = charCodes[Math.floor(Math.random() * characterAmount)]
-    passwordCharacters.push(String.fromCharCode(characterCode))
-}
-return passwordCharacters.join()
-
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
   
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
+}
 
-// function arrayFromLowToHigh(low, high) {
-//   const array = []
-//   for (let i = low; i <= high; i++) {
-//   array.push(i)
-//   }
-//   return array
-// }
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
 
-// function syncCharacterAmount(e) {
-//   const value = e.target.value
-//   characterAmountNumber.value = value
-//   characterAmountRange.value = value
-// }
-
-
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
 
 
 
